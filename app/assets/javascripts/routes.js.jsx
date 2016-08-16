c30af4584a1ee332d90163cfa45953f4744e5188
Wataru Kay <wata1991@gmail.com>
2016-08-17 00:02:31 +0900

    Add Default Route and URL prop

--- app/assets/javascripts/routes.js.jsx
+++ app/assets/javascripts/routes.js.jsx
@@ -1,7 +1,8 @@
-var Route = ReactRouter.Route;
+const Route = ReactRouter.Route, DefaultRoute = ReactRouter.DefaultRoute;
 
 this.MyRoutes = (
   <Route handler={App}>
+    <DefaultRoute handler={HelloWorld} />
     <Route name='signup ' handler={SignUpForm} path='/signup' />
     <Route name='users' handler={User} path='/user' />
   </Route>
