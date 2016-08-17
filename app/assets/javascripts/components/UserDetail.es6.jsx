8fcf640b649ee54bb8486024d325c5386aaeaf33
Wataru Kay <wata1991@gmail.com>
2016-08-18 00:03:33 +0900

    User Detail shows Skill Vote Count & Voters

--- app/assets/javascripts/components/UserDetail.es6.jsx
+++ app/assets/javascripts/components/UserDetail.es6.jsx
@@ -18,18 +18,42 @@ class UserDetail extends React.Component {
   }
 
   render() {
-    let unique = [...new Set(this.state.user.skills.map(skill => skill.name))];
-    const skills = this.state.user.skills.map( skill => {
-      const count = this.state.user.skills.filter( s => {
-        if(s.name == skill.name) return s
-      })
-      return(
-        <div key={skill.id}>
-          <li> Skill Name: {skill.name} </li>
-          <li> Skill Votes: {count.length} </li>
-        </div>
-          )
+    let skills = this.state.user.skills
+    // get all unique skill names
+    const unique = [...new Set(skills.map(skill => skill.name))];
+    // restructure json object to include vote count & voters
+    const finalSkills = unique.map(uniqueSkill => {
+      let count = 0;
+      let voters = [];
+      for(let i =0; i<skills.length; i++) {
+        if(skills[i].name == uniqueSkill) {
+          count++;
+          let voter = {name:skills[i].assignee_name, id:skills[i].assignee_id}
+          voters.push(voter)
+        }
+      }
+      let result = {skillName:uniqueSkill, voteCount:count,voters:voters}
+      return result
     })
+    
+    skills2 = finalSkills.map( skill => {
+      const allVoters = skill.voters.map( voter => {
+        return(
+            <li id={voter.id}>Voter: {voter.name}</li>
+        )
+      })
+
+      return(
+        <div>
+          <li>Skill Name: {skill.skillName}</li>
+          <li>Votes: {skill.voteCount}</li>
+          <ul>
+            {allVoters}
+          </ul>
+        </div>
+      )
+    })
+
     return(
       <div>
         User Detail for {this.state.user.name}
@@ -39,7 +63,7 @@ class UserDetail extends React.Component {
           <li> Age: {this.state.user.age} </li>
           <li> Job: {this.state.user.job} </li>
           <h2> Skills </h2>
-          {skills}
+          {skills2}
         </ul>
       </div>
     )
