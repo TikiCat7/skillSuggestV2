9e2d759a3e7baeb061ab9d3e4a5aaf829661b915
Wataru Kay <wata1991@gmail.com>
2016-08-22 19:42:28 +0900

    Add User Password & Registration Form

--- app/assets/javascripts/components/SignUpForm.es6.jsx
+++ app/assets/javascripts/components/SignUpForm.es6.jsx
@@ -6,11 +6,31 @@ class SignUpForm extends React.Component {
   render() {
     return(
       <div>
-        <div>Sign Up</div>
-        Name: <input type='text' placeholder='enter Name'></input>
-        Age: <input type='text' placeholder='enter Age'></input>
-        Job: <input type='text' placeholder='enter Job'></input>
-        <button type='submit'>Submit</button>
+        <form role='form' acceptCharset="UTF-8" action='/api/users' method='post' className="new_user" id="new_user">
+          <input name="utf8" type="hidden" value="&#x2713;" />
+          <input type='hidden' name='authenticity_token' value={this.props.authenticity_token} />
+          <div>
+            Sign Up
+          </div>
+          <label htmlFor="user_name">Name</label>
+          <input id="user_name" name="user[name]" type="text" />
+
+          <label htmlFor="user_age">Age</label>
+          <input id="user_age" name="user[age]" type="integer" />
+
+          <label htmlFor="user_job">Job</label>
+          <input id="user_job" name="user[job]" type="text" />
+
+          <label htmlFor="user_password">Password</label>
+          <input id="user_password" name="user[password]"
+            type="password" />
+
+          <label htmlFor="user_password_confirmation">Confirmation</label>
+          <input id="user_password_confirmation"
+            name="user[password_confirmation]" type="password" />
+
+          <button type='submit'>Register Account</button>
+        </form>
       </div>
     )
   }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815122251) do
+ActiveRecord::Schema.define(version: 20160822094520) do
 
   create_table "skills", force: :cascade do |t|
     t.string   "name"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20160815122251) do
     t.string   "name"
     t.string   "job"
     t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
