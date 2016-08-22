6fa2d98af07bc83f209f16bcfc154a4855765f7d
Wataru Kay <wata1991@gmail.com>
2016-08-22 19:57:49 +0900

    Return Error JSON for failed sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :skills, dependent: :destroy
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :job, presence: true
   has_secure_password
 end
