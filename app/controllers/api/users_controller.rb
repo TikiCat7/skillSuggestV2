746c85c8d71871ad1dcece6e52f623bea49c1051
Wataru Kay <wata1991@gmail.com>
2016-08-22 19:57:49 +0900

    Return Error JSON for failed sign-up

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -16,9 +16,9 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user
+      render json: @user, status: 200
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { messages: @user.errors.full_messages }, status: 400
     end
   end
 
