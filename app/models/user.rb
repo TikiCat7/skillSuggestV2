374101c1517859be82324cab459591cc2bd71bfd
Wataru Kay <wata1991@gmail.com>
2016-08-22 23:55:00 +0900

    Add length limitation to user params

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :skills, dependent: :destroy
-  validates :name, presence: true
-  validates :age, presence: true
-  validates :job, presence: true
+  validates :name, presence: true, length: {maximum: 50}
+  validates :age, presence: true, length: {maximum: 3}
+  validates :job, presence: true, length: {maximum: 50}
   has_secure_password
 end
