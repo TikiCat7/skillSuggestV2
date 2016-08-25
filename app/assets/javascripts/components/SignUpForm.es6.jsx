575ba244e42bae096bc6c5a4c51f0a01c273a1f6
Wataru Kay <wata1991@gmail.com>
2016-08-25 23:07:19 +0900

    Fix use of authenticity token, use axios to post

--- app/assets/javascripts/components/SignUpForm.es6.jsx
+++ app/assets/javascripts/components/SignUpForm.es6.jsx
@@ -2,42 +2,74 @@ class SignUpForm extends React.Component {
   constructor(){
     super()
     this.state = {
-      errorMessage:[]
+      errorMessage:[],
+      formName:'',
+      formAge:'',
+      formJob:'',
+      formPassword:'',
+      formPassword_confirmation:''
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleNameChange = this.handleNameChange.bind(this)
+    this.handleAgeChange = this.handleAgeChange.bind(this)
+    this.handleJobChange = this.handleJobChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handlePasswordConfirmationChange = this.handlePasswordConfirmationChange.bind(this)
+  }
+
+  handleNameChange(event) {
+    event.preventDefault()
+    this.setState({formName:event.target.value})
+  }
+
+  handleAgeChange(event) {
+    event.preventDefault()
+    this.setState({formAge:event.target.value})
+  }
+
+  handleJobChange(event) {
+    event.preventDefault()
+    this.setState({formJob:event.target.value})
+  }
+
+  handlePasswordChange(event) {
+    event.preventDefault()
+    this.setState({formPassword:event.target.value})
+  }
+
+  handlePasswordConfirmationChange(event) {
+    event.preventDefault()
+    this.setState({formPassword_confirmation:event.target.value})
   }
 
   handleSubmit(event){
     event.preventDefault()
-    const name = this.refs.user_name.value
-    const age = this.refs.user_age.value
-    const job = this.refs.user_job.value
-    const password = this.refs.user_password.value
-    const password_confirmation = this.refs.user_password_confirmation.value
 
-    fetch('/api/users', {
-      method: 'post',
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-      body: `utf8=%E2%9C%93&authenticity_token=${this.props.authenticity_token}&user%5Bname%5D=${name}&user%5Bage%5D=${age}&user%5Bjob%5D=${job}&user%5Bpassword%5D=${password}&user%5Bpassword_confirmation%5D=${password_confirmation}`
-    })
-    .then(response => {
-      if (response.status === 400) {
-        response.json().then(object => {
-        console.log('Something went wrong with creating a user: ')
-        console.log(object.messages)
-        this.setState({errorMessage:object.messages})
-      })
-      } else if (response.status === 200) {
-          this.setState({errorMessage:[]})
-          response.json().then(object => {
-          console.log('User successfully created!')
-          console.log(object)
-          location.href = `/#/user/${object.id}`
-        })
+    const body = {
+      authenticity_token:this.props.authenticity_token,
+      user: {
+        name:this.state.formName,
+        age:this.state.formAge,
+        job:this.state.formJob,
+        password:this.state.formPassword,
+        password_confirmation:this.state.formPassword_confirmation
       }
-    })
-  }
+    }
+
+    axios.post('/api/users', body)
+      .then(response => {
+        console.log(response)
+        const user = {
+          name:response.data.name,
+          id:response.data.id
+        }
+      this.props.handleUserUpdate(user)
+      location.href = `/#/user/${response.data.id}`
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
 
   render() {
     const errorMessage = this.state.errorMessage.map((error,index) => {
@@ -51,28 +83,28 @@ class SignUpForm extends React.Component {
         <div>
           {errorMessage}
         </div>
-        <form role='form' acceptCharset="UTF-8" action='/api/users' method='post' className="new_user" id="new_user" onSubmit={this.handleSubmit.bind(this)}>
+        <form role='form' acceptCharset="UTF-8" action='/api/users' method='post' className="new_user" id="new_user" onSubmit={this.handleSubmit}>
           <input name="utf8" type="hidden" value="&#x2713;" />
           <input type='hidden' name='authenticity_token' value={this.props.authenticity_token} />
           <div>
             Sign Up
           </div>
           <label htmlFor="user_name">Name
-            <input ref="user_name" id="user_name" name="user[name]" type="text" />
+            <input ref="user_name" id="user_name" name="user[name]" type="text" onChange={this.handleNameChange} />
           </label>
           <label htmlFor="user_age">Age
-            <input ref="user_age" id="user_age" name="user[age]" type="integer" />
+            <input ref="user_age" id="user_age" name="user[age]" type="integer" onChange={this.handleAgeChange}/>
           </label>
           <label htmlFor="user_job">Job
-            <input ref="user_job" id="user_job" name="user[job]" type="text" />
+            <input ref="user_job" id="user_job" name="user[job]" type="text" onChange={this.handleJobChange}/>
           </label>
           <label htmlFor="user_password">Password
             <input ref="user_password" id="user_password" name="user[password]"
-              type="password" />
+              type="password" onChange={this.handlePasswordChange}/>
           </label>
           <label htmlFor="user_password_confirmation">Confirmation
             <input ref="user_password_confirmation" id="user_password_confirmation"
-              name="user[password_confirmation]" type="password" />
+              name="user[password_confirmation]" type="password" onChange={this.handlePasswordConfirmationChange}/>
           </label>
           <button type='submit'>Register Account</button>
         </form>
