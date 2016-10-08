a867c4384ab4a02a22975dd928f0893abe865244
Wataru Kay <wata1991@gmail.com>
2016-10-08 23:49:01 +0900

    POSTGRESQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160822094520) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "skills", force: :cascade do |t|
     t.string   "name"
     t.integer  "assignee_id"
@@ -19,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160822094520) do
     t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["user_id"], name: "index_skills_on_user_id"
+    t.index ["user_id"], name: "index_skills_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +34,5 @@ ActiveRecord::Schema.define(version: 20160822094520) do
     t.string   "password_digest"
   end
 
+  add_foreign_key "skills", "users"
 end
